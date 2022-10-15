a173cc584dbde43c209b7544c6630e743b93562b
agon.a <agon.a@gjirafa.com>
2022-10-15 17:11:47 +0200

    removed unused components

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand>TRUDO-NEWS</b-navbar-brand>
+
+    <b-navbar-nav>
+      <b-nav-item-dropdown right>
+        <template #button-content>
+          <em>User</em>
+        </template>
+        <b-dropdown-item href="#">Profile</b-dropdown-item>
+        <b-dropdown-item v-if="true" href="/auth">Login</b-dropdown-item>
+        <b-dropdown-item v-else href="#">Sign Out</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import News from "../views/News.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "news",
+    component: News,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/auth",
+    name: "UserAuth",
+    component: () => import("../views/UserAuth.vue"),
   },
 ];
 
