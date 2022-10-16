4bbed3bcd9d88a1fbbd217436e2f695296088430
agon.a <agon.a@gjirafa.com>
2022-10-16 19:12:48 +0200

    delete from userNews

--- src/views/UserNews.vue
+++ src/views/UserNews.vue
@@ -3,6 +3,24 @@
     <b-card class="my-50" no-body>
       <news-table :data="userNews" :is-loading="isLoading" />
     </b-card>
+    <b-modal
+      id="modal-small-delete"
+      ok-title="Delete"
+      cancel-title="Cancel"
+      ok-variant="danger"
+      cancel-variant="outline-secondary"
+      centered
+      size="md"
+      title="Delete News"
+      no-enforce-focus
+      :hide-footer="!rowToDelete.isRemovable"
+      @ok="triggerDelete"
+    >
+      <span v-if="rowToDelete.isRemovable">
+        Are you sure you want to delete this
+      </span>
+      <span v-else>You can not delete this news :(</span>
+    </b-modal>
   </div>
 </template>
 
@@ -29,6 +47,9 @@ export default {
       const userNews = news?.filter((news) => news.authorId === userId);
       return userNews;
     },
+    rowToDelete() {
+      return this.$store.getters.rowToDelete;
+    },
   },
   methods: {
     async getAllNews() {
@@ -39,6 +60,15 @@ export default {
         console.log(err);
       } finally {
         this.isLoading = false;
+      }
+    },
+    async triggerDelete() {
+      try {
+        await this.$store.dispatch("deleteNews", this.rowToDelete);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        await this.getAllNews();
       }
     },
   },
